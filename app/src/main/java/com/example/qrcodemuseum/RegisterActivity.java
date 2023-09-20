eeb80b1944895b7a0f47d6c85ddd432f115b3852
Vitor Kato <vitor48kato@gmail.com>
2023-09-20 16:18:44 -0300

    Add validation, radioButton and user retrieve, add more fields in User class

--- app/src/main/java/com/example/qrcodemuseum/RegisterActivity.java
+++ app/src/main/java/com/example/qrcodemuseum/RegisterActivity.java
@@ -5,22 +5,98 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.example.qrcodemuseum.model.User;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    //Components
+    private EditText userName;
+    private EditText email;
+    private EditText phone;
+    private EditText password;
+    private EditText confirmPassword;
+    private RadioGroup userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        userName = findViewById(R.id.RegisterEditTextUser);
+        email = findViewById(R.id.RegisterEditTextEmail);
+        phone = findViewById(R.id.RegisterEditTextPhone);
+        password = findViewById(R.id.RegisterEditTextPassword);
+        confirmPassword = findViewById(R.id.RegisterEditTextConfirmPassword);
+        userType = findViewById(R.id.RegisterRadioGroup);
+
     }
 
     public void register(View view) {
-        Toast.makeText(
-                getApplicationContext(),
-                "Register",
-                Toast.LENGTH_LONG
-        ).show();
+
+        if (userName.getText().length() < 1 || email.getText().length() < 1 || phone.getText().length() < 1 ||
+                password.getText().length() < 1 || confirmPassword.getText().length() < 1 || userType.getCheckedRadioButtonId() == -1)
+        {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Please fill all the fields.",
+                    Toast.LENGTH_LONG
+            ).show();
+        } else {
+            //Todo:Create new user on database, get the last id on database and +1 save
+
+            //Check radioButton
+            RadioButton selectedRadioButton = findViewById(userType.getCheckedRadioButtonId());
+
+            //TODO:Verify passwords match
+            //        if(senha.getText().toString().length() < 6 || confirmarSenha.getText().toString().length() < 6)
+            //        {
+            //            Toast.makeText(
+            //                    getApplicationContext(),
+            //                    "Digite uma senha de pelo menos 6 dígitos!",
+            //                    Toast.LENGTH_LONG
+            //            ).show();
+            //        }
+            //        else if(!senha.getText().toString().equals(confirmarSenha.getText().toString()))
+            //        {
+            //            Toast.makeText(
+            //                    getApplicationContext(),
+            //                    "As senhas não correspondem!",
+            //                    Toast.LENGTH_LONG
+            //            ).show();
+            //        }
+            //        else
+            //        {
+            //            armazenarDadosUsuario();
+            //
+            //            Intent intent = new Intent(getApplicationContext(), Cadastro3Activity.class);
+            //
+            //            intent.putExtra("usuario", usuario);
+            //
+            //            startActivity(intent);
+            //            finish();
+            //        }
+
+            Integer userTypeNumber = (selectedRadioButton.getText().toString().equals("Student")) ? 2 : 1;
+
+            User user = new User(2, userName.getText().toString(), email.getText().toString(),
+                    phone.getText().toString(), password.getText().toString(), userTypeNumber);
+
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Registered: " + user.getUser() + " " + user.getEmail() + " " + user.getPhone() + " "
+                            + user.getPassword() + " " + user.getId() + " " + user.getUserType(),
+                    Toast.LENGTH_LONG
+            ).show();
+
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     @Override
--- app/src/main/java/com/example/qrcodemuseum/model/User.java
+++ app/src/main/java/com/example/qrcodemuseum/model/User.java
@@ -6,16 +6,22 @@ public class User implements Serializable {
 
     private Integer id;
     private String user;
+    private String email;
+    private String phone;
     private String password;
+    private Integer userType; //1 - Teacher, 2 - Student, 3 - Visitor, 4 - Adm
 
     private User() {
 
     }
 
-    public User(Integer id, String user, String password) {
+    public User(Integer id, String user, String email, String phone, String password, Integer userType) {
         this.id = id;
         this.user = user;
+        this.email = email;
+        this.phone = phone;
         this.password = password;
+        this.userType = userType;
     }
 
     public Integer getId() {
@@ -34,11 +40,35 @@ public class User implements Serializable {
         this.user = user;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Integer getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Integer userType) {
+        this.userType = userType;
     }
 }
